c39a799c35f90a98abc121b311514b89076215aa
sameerswar <dino7bittu@gmail.com>
2026-05-14 09:01:20 +0530

    Add files via upload

---
+++ auth/auth.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvu--M7QJEV4_zxfd2cGaDOQdeQRV1Y1w",
+  authDomain: "sameerswaraj-7a059.firebaseapp.com",
+  projectId: "sameerswaraj-7a059",
+  storageBucket: "sameerswaraj-7a059.firebasestorage.app",
+  messagingSenderId: "401397860957",
+  appId: "1:401397860957:web:83e08e8198ccf19a58dbb4",
+  measurementId: "G-0YD08B11SD"
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
+
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const siteRoot = new URL("../", import.meta.url);
+const authPage = new URL("auth/auth.html", siteRoot).href;
+const homePage = new URL("index.html", siteRoot).href;
+
+window.loginGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then(() => {
+      const nextPage = sessionStorage.getItem("nextPage") || homePage;
+      window.location.href = nextPage;
+    })
+    .catch(err => alert(err.message));
+};
+
+window.checkAuth = () => {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      sessionStorage.setItem("nextPage", window.location.href);
+      window.location.href = authPage;
+    }
+  });
+};
+
+window.logoutUser = () => {
+  signOut(auth).then(() => {
+    window.location.href = authPage;
+  });
+};
---
+++ splash/globalsplash.js
@@ -0,0 +1,26 @@
+const splashScriptUrl = document.currentScript.src;
+const currentPage = window.location.pathname;
+
+// check if already splash page
+const isSplash =
+currentPage.includes("splash/index.html");
+
+// splash already shown?
+const splashShown =
+sessionStorage.getItem("splashShown");
+
+if (!isSplash && !splashShown) {
+
+    // mark splash shown
+    sessionStorage.setItem("splashShown", "true");
+
+    // save destination
+    sessionStorage.setItem(
+        "nextPage",
+        window.location.href
+    );
+
+    // IMPORTANT:
+    // relative path use karo
+    window.location.href = new URL("index.html", splashScriptUrl).href;
+}
